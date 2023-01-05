713e4ad1f6c2294969343d66998d91ce6b171718
GerardTorres1 <a20194793@pucp.edu.pe>
2023-01-05 17:38:34 -0500

    whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic", # use whitenoise to serve static files
     "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
